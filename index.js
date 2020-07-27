19c207c707b0d6922e1425e210a2e461b073655c
Naiga72 <stezona72@gmail.com>
2020-07-27 23:48:07 +0300

    signin

---
+++ index.js
@@ -0,0 +1,22 @@
+var attempt = 3; //variable to count number of attempts.
+// below function executes on click of login button.
+function validated(){
+    var username=document.getElementById("username").Value;
+    var password =documeElementById(password).Value;
+    var Email =document.getElementById("password").Value;
+    if(username=="Generous" && password=="123456789"){
+    alert("Login successfully");
+    window.location ="https://www.pivotaltracker.com/n/projects/2458544"; // redirecting to other page.
+    return false
+    }
+    else{
+        attempt--;//decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+        //Disabling fields after 3 attempts.
+        if(attempt == 0){
+            document.getElementById("username").disabled =true;
+            document.getElementById("password").disabled =true;
+            document.getElementById("submit").disabled =true;
+        }
+    }
+}
