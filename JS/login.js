5115a11ca1ce6ae3a609bad3631d7d9b0ae5a29f
SamuelMabonga <samuelmabonga256@gmail.com>
2020-07-29 09:41:42 +0300

    Fixed login and signup

---
+++ JS/login.js
@@ -0,0 +1,54 @@
+// the login in page
+const users = [
+  {
+    username:'samuel',
+    password:'pass'
+  },
+  {
+    username:'priscilla',
+    password:'pass'
+  }
+];
+
+
+let error = document.querySelector(".error");
+let form = document.querySelector("form");
+
+function login() {
+
+  console.log("Running")
+
+  // access users list form localStorage
+  let user = JSON.parse(localStorage.getItem("users", users));
+  console.log(users);
+
+  // grab input field values
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  // user validation
+  for (let i = 0; i < user.length; i++) {
+    if (username === user[i].username && password === user[i].password) {
+      alert("Login successfull")
+      localStorage.setItem('userLoggedIn', JSON.stringify(username))
+      // console.log(window.location)
+      window.location = 'index.html'
+      return false
+    } else {
+      error.style.visibility = "visible";
+      setTimeout(() => {
+        error.style.visibility = "hidden";
+      }, 5000);
+      form.reset();
+      return;
+    }
+  }
+}
+
+let btnSignUp = document.querySelector("#login");
+  
+btnSignUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  login();
+
+});
---
+++ JS/signup.js
@@ -0,0 +1,49 @@
+// SIGN UP
+
+// Initial users
+const users = [
+  {
+    username:'samuel',
+    password:'pass'
+  },
+  {
+    username:'priscilla',
+    password:'pass'
+  }
+];
+  
+function signUp() {
+  console.log('Validating')
+
+  let fname = document.querySelector("#fname").value;
+  let lname = document.querySelector("#lname").value;
+  let phone = document.querySelector("#phone").value;
+  let email = document.querySelector("#email").value;
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+
+  let user = {
+    firstname: fname,
+    lastname: lname,
+    phoneNumber: phone,
+    email: email,
+    username: username,
+    password: password
+  };
+  
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert('Sign up successful!')
+  console.log(window.location)
+  window.location = "index.html"
+}
+  
+let btnSignUp = document.querySelector("#signup");
+  
+btnSignUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  signUp();
+
+});
+
